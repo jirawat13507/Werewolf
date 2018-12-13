31bbb8c50bfb3a5428df20acece72d17c23f3589
JIRAWAT WONGWICHAI <jirawat_wongwichai@cmu.ac.th>
2018-12-13 18:39:34 +0700

    รูปหน้าจอ login

--- WerewolfClient/Login.cs
+++ WerewolfClient/Login.cs
@@ -72,5 +72,10 @@ namespace WerewolfClient
             wcmd.Payloads = new Dictionary<string, string>() { { "Login", TbLogin.Text}, { "Password",TbPassword.Text}, { "Server", TBServer.Text } };
             controller.ActionPerformed(wcmd);
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
